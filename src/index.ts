c132891c8660def25b1d46a846c81a8234de0ac0
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-12-26 23:59:14 +0900

    zennのmeta情報を取得してみる

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/welcome", async (c) => {
+  // http://zenn.dev/ の情報を取得
+  // metaデータを取得
+  const response = await fetch("http://zenn.dev/");
+  const body = await response.text();
+  const meta = body.match(/<meta.*?>/g);
+  return c.body(meta ? meta.join("\n") : "No meta tags found");
+});
 
-export default app
+app.get("/", (c) => {
+  return c.body("Hello, World!");
+});
+
+export default app;
