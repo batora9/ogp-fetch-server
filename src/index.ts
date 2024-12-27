aae9c2ac1238fc18ef5eeddf7e760a9aca52d590
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-12-27 13:32:57 +0900

    imageも追加

--- src/index.ts
+++ src/index.ts
@@ -23,15 +23,18 @@ app.get("/", async (c) => {
     const faviconMatch = meta.match(/<link rel="icon" href="(.*?)"/);
     const descriptionMatch = meta.match(/<meta name="description" content="(.*?)"/);
     const titleMatch = meta.match(/<title>(.*?)<\/title>/);
+    const imageMatch = meta.match(/<meta property="og:image" content="(.*?)"/);
 
     // Get the base URL
     const baseUrl = new URL(url);
     const faviconUrl = faviconMatch ? new URL(faviconMatch[1], baseUrl).toString() : null;
+    const imageUrl = imageMatch ? new URL(imageMatch[1], baseUrl).toString() : null;
 
     const result = {
       title: titleMatch ? titleMatch[1] : null,
       description: descriptionMatch ? descriptionMatch[1] : null,
       favicon: faviconUrl,
+      image: imageUrl,
     };
 
     return c.json(result);
