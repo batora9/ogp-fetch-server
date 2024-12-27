6cef94be26800281af91bfb7571267226f763ed8
batora9 <sm.net_minecraft.hacker@outlook.jp>
2024-12-27 12:11:51 +0900

    batoran.comのtitle,favicon,descriptionを取得

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,21 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/welcome", async (c) => {
-  // http://zenn.dev/ の情報を取得
-  // metaデータを取得
-  const response = await fetch("http://zenn.dev/");
-  const body = await response.text();
-  const meta = body.match(/<meta.*?>/g);
-  return c.body(meta ? meta.join("\n") : "No meta tags found");
+  const url = "https://batoran.com";
+  const response = await fetch(url);
+  const meta = await response.text();
+  const favicon = meta.match(/<link rel="icon" href="(.*?)"/);
+  const description = meta.match(/<meta name="description" content="(.*?)"/);
+  const title = meta.match(/<title>(.*?)<\/title>/);
+  if (title && favicon && description) {
+    return c.json({
+      title: title[1],
+      description: description[1],
+      favicon: url + favicon[1],
+    });
+  } else {
+    console.log("Title not found");
+  }
 });
 
 app.get("/", (c) => {
